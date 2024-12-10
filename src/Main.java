f3eed44b38f91a4b33be86eaaa73ec717ec03eda
Saksham Dube <sakshamdube17@gmail.com>
2024-12-10 17:15:56 +0530

    added new comment

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,15 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        //check
+
+
+//         hello
+        
          car c = new car();
          car m = new mercedes();
          m.run();
+         Coc coc = new Coc();
+         coc.run();
     }
 }
